1c21247891a0771d1b09e485ebab669828186c99
ftambler <federicotambler@gmail.com>
2024-09-28 22:27:08 -0300

    Get Process Thread by Status

--- models/Order.java
+++ models/Order.java
@@ -1,6 +1,13 @@
 package models;
 
-public class Order {
+import java.util.concurrent.PriorityBlockingQueue;
+
+import runnables.ProcessDelivery;
+import runnables.ProcessOrder;
+import runnables.ProcessPackaging;
+import runnables.ProcessPayment;
+
+public class Order implements Comparable<Order>{
     private String owner;
     private double cost;
     private Status status;
@@ -32,5 +39,26 @@ public class Order {
     public String getOwner() {
         return owner;
     }
+
+    @Override
+    public int compareTo(Order o) {
+        return Integer.compare(this.priority, o.getPriority());
+    }
    
+    public static ProcessOrder createOrder(Order currentOrder, PriorityBlockingQueue<Order> outQueue){
+        switch (currentOrder.getStatus()) {
+            case TOPROCESS:
+                return new ProcessPayment(currentOrder, outQueue);
+
+            case TOPACKAGE:
+                return new ProcessPackaging(currentOrder, outQueue);
+
+            case TODELIVER:
+                return new ProcessDelivery(currentOrder, outQueue);
+            
+            default:
+                return null;
+                
+        }
+    }
 }
