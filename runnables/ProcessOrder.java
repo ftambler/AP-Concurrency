fb2708d653630462d47e6b5bc105df1d86c13ed3
ftambler <federicotambler@gmail.com>
2024-09-28 22:34:04 -0300

    PBQ & Random Wait

--- runnables/ProcessOrder.java
+++ runnables/ProcessOrder.java
@@ -1,27 +1,33 @@
 package runnables;
 
+import java.util.Random;
+import java.util.concurrent.PriorityBlockingQueue;
+
 import models.Order;
 import models.Status;
 
-public class ProcessOrder implements Runnable{
+public class ProcessOrder extends Thread{
 
     private Order order;
+    private PriorityBlockingQueue<Order> outQueue;
 
-    public ProcessOrder(Order order) {
+    public ProcessOrder(Order order, PriorityBlockingQueue<Order> outQueue) {
         this.order = order;
+        this.outQueue = outQueue;
     }
 
     @Override
     public void run() {
-        
         try {
-            Thread.sleep(1000);
+            Random rnd =  new Random();
+            Thread.sleep(rnd.nextInt(1000) + 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        getOrder().setStatus( Status.getNext( getOrder().getStatus()) );
+        this.order.setStatus( Status.getNext( this.order.getStatus()) );
 
+        outQueue.add(order);
 
     }
 
